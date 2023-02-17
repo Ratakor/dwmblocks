5fe2a107137ffd7c7ac51da5e93a922668e02cf7
Ratakor <contact@ratakor.com>
2023-02-17 22:46:34 +0100

    damn it's pretty hard to make it works with youtube playlists

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/  /*Command*/         /*Update Interval*/    /*Update Signal*/
-    {"",      "sb_music",         5,                     9},
+    {"",      "sb_music",         120,                   9},
     {"",      "sb_cputemp",       10,                    0},
     /*{"",      "sb_fanspeed",      10,                    0},*/
     /*{"",      "sb_memory",        10,                    0},*/
